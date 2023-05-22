a48e34e91c2a568faf7ba03ae4a50fa9e3c3fb71
Shubham <shubham@e-arth.in>
2023-05-22 14:10:56 +0530

    teacher form completed

--- src/componants/Forms.js
+++ src/componants/Forms.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import PersnolInfo from './forms/PersnolInfo'
-
+import React from "react";
+// import PersnolInfo from "./forms/PersnolInfo";
+import TeacherForm from "./forms/TeacherForm";
 export default function Forms() {
   return (
     <div>
-      <PersnolInfo />
+      {/* <PersnolInfo /> */}
+      <TeacherForm />
     </div>
-  )
+  );
 }
