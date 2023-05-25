a3a3b719fb6b82bbdade907b0bc3558bd79b2361
priyanka <priyankasbirajdar3412@gmail.com>
2023-05-25 14:29:10 +0530

    router add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import Student from "./componants/Student";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminLogin from "./componants/Admin/AdminLogin";
+import GradeForm from "./componants/forms/GradeForm";
+import TeacherForm from "./componants/forms/TeacherForm";
+import PersonalInfo from "./componants/forms/PersnolInfo";
 function App() {
-  return (
-    <div className="App">
-          <Student />
-      <BrowserRouter>
-        <Routes>
-
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AdminLogin />}></Route>
+                    <Route path="/teacher" element={<TeacherForm/>}></Route>
+                    <Route path="/grade" element={<GradeForm/>}></Route>
+                    <Route path="/personal" element={<PersonalInfo/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
