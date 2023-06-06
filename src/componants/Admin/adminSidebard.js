0239cbb5519e38e84e5c1307594be91b66030760
Punam Nale <punamnale12@gmail.com>
2023-06-06 13:27:37 +0530

    Sidebar

--- src/componants/Admin/adminSidebard.js
+++ src/componants/Admin/adminSidebard.js
@@ -1,17 +1,12 @@
 /** @format */
 
 import React from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import * as RxIcons from "react-icons/rx";
-import * as MdIcons from "react-icons/md";
-import * as TbIcons from "react-icons/tb";
-import * as BsIcons from "react-icons/bs";
 import { MdDashboard } from "react-icons/md";
 import { RxAvatar } from "react-icons/rx";
 import { FaChalkboardTeacher, FaBookOpen } from "react-icons/fa";
 import { AiFillQuestionCircle, AiOutlineLogout } from "react-icons/ai";
 import { TbReportSearch } from "react-icons/tb";
+import * as BsIcons from "react-icons/bs";
 
 function Adminsidebar() {
   return (
@@ -33,57 +28,6 @@ function Adminsidebar() {
       >
         <section className="sidebar">
           <div className="user-panel">
-            <ul
-              style={{ paddingInlineStart: 11 }}
-              className="sidebar-menu 
-                        active menu-open tree"
-            >
-              <li>
-                <MdIcons.MdDashboard />
-                <a href="/admin-dashboard">
-                  <sapn>Dashboard</sapn>
-                </a>
-              </li>
-              <li>
-                <RxIcons.RxAvatar />
-                <a href="/student">
-                  <span>Student</span>
-                </a>
-              </li>
-              <li>
-                <FaIcons.FaChalkboardTeacher />
-                <a href="/teacher">
-                  <span>Teacher</span>
-                </a>
-              </li>
-              <li>
-                <FaIcons.FaBookOpen />
-                <a href="/grade">Grade</a>
-              </li>
-              <li>
-                <BsIcons.BsFillPersonCheckFill />
-                <a href="/allocate-user">Allocation</a>
-              </li>
-
-              <li>
-                <AiIcons.AiFillQuestionCircle />
-                <a href="assesment">
-                  <sapn>Asssement</sapn>
-                </a>
-              </li>
-              <li>
-                <TbIcons.TbReportSearch />
-                <a href="report">
-                  <span>Report</span>
-                </a>
-              </li>
-              <li>
-                <AiIcons.AiOutlineLogout />
-                <a href="/">
-                  <span className="margin-right-30">Logout</span>
-                </a>
-              </li>
-              {/*  <? php}?> */}
             <ul className="sidebar-menu active menu-open tree">
               <li>
                 <a href="/admin-dashboard">
@@ -104,6 +48,10 @@ function Adminsidebar() {
                 <a href="/grade">
                   <FaBookOpen /> Grade
                 </a>
+              </li>
+              <li>
+                <BsIcons.BsFillPersonCheckFill />
+                <a href="/allocate-user">Allocation</a>
               </li>
               <li>
                 <a href="/grade">
